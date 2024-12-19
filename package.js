7d308d66ee4e5413947787f00b5d5246d76cc302
jankapunkt <jkuester@uni-bremen.de>
2024-12-19 14:48:37 +0100

    migration(core): upgrade package to support Meteor 1/2/3

--- package.js
+++ package.js
@@ -5,12 +5,12 @@
 Package.describe({
   name: 'seba:method-hooks',
   summary: 'Provides before/after hooks for Meteor methods',
-  version: '3.0.4',
+  version: '4.0.0-rc.0',
   git: 'https://github.com/Meteor-Community-Packages/meteor-method-hooks',
 });
 
 Package.onUse(function definePackage(api) {
-  api.versionsFrom('1.12.1');
+  api.versionsFrom(['1.12.1', '2.8', '3.0']);
   api.use('meteor');
   api.use('ecmascript');
   api.mainModule('server.js', 'server');
